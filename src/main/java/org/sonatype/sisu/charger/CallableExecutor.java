64db548cf6b7c8cffd9a52a7d1de06e3ef231d0d
Tamas Cservenak <tamas@cservenak.net>
2011-08-16 22:18:12 +0200

    Javadoc

--- src/main/java/org/sonatype/sisu/charger/CallableExecutor.java
+++ src/main/java/org/sonatype/sisu/charger/CallableExecutor.java
@@ -2,8 +2,18 @@ package org.sonatype.sisu.charger;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
+import java.util.concurrent.RejectedExecutionException;
 
 public interface CallableExecutor
 {
-    <T> Future<T> submit( Callable<T> task );
+    /**
+     * Submits a value-returning task for execution and returns a Future representing the pending results of the task.
+     * 
+     * @param task the task to submit
+     * @return a Future representing pending completion of the task
+     * @throws RejectedExecutionException if the task cannot be scheduled for execution
+     * @throws NullPointerException if the task is null
+     */
+    <T> Future<T> submit( Callable<T> task )
+        throws RejectedExecutionException, NullPointerException;
 }
