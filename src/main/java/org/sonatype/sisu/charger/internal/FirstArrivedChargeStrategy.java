0264e7edae0b08b39f37476fa2a27de20e8de2c2
Benjamin Hanzelmann <ben@nabcos.de>
2011-08-17 12:42:37 +0200

    o Fix strategy to work for multiple concurrent charges

--- src/main/java/org/sonatype/sisu/charger/internal/FirstArrivedChargeStrategy.java
+++ src/main/java/org/sonatype/sisu/charger/internal/FirstArrivedChargeStrategy.java
@@ -1,32 +1,57 @@
 package org.sonatype.sisu.charger.internal;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
 /**
  * ChargeStrategy for "first with payload or unhandled exception". This strategy will block as long as first Callable
  * delivers some payload or fails with unhandled exception -- making whole Charge to fail. In case of "bail out", the
  * next Callable is processed in same way, as long as there are Callables.
- * 
- * @author cstamas
+ *
  * @param <E>
+ * @author cstamas
  */
 public class FirstArrivedChargeStrategy<E>
     extends AbstractChargeStrategy<E>
 {
-    private volatile ChargeWrapper<E> first = null;
+    private Map<Charge<?>, ChargeState> states = new HashMap<Charge<?>, ChargeState>( 4 );
 
-    private CountDownLatch firstFound = new CountDownLatch( 1 );
+    private static class ChargeState<E>
+    {
+        CountDownLatch latch;
+
+        ChargeWrapper<E> wrapper;
+
+        private ChargeState()
+        {
+            this.latch = new CountDownLatch( 1 );
+        }
+
+        private ChargeState( ChargeWrapper<E> wrapper )
+        {
+            this.latch = new CountDownLatch( 0 );
+            this.wrapper = wrapper;
+        }
+    }
 
     @Override
-    public boolean isDone( final Charge<E> charge, final ChargeWrapper<E> wrapper )
+    public void setDone( final Charge<E> charge, final ChargeWrapper<E> wrapper )
     {
-        first = wrapper;
+        ChargeState<E> state;
 
-        firstFound.countDown();
+        synchronized ( states )
+        {
+            if ( ( state = states.get( charge ) ) == null )
+            {
+                states.put( charge, ( state = new ChargeState( wrapper ) ) );
+            }
 
-        return true;
+            state.wrapper = wrapper;
+            state.latch.countDown();
+        }
     }
 
     @Override
@@ -39,9 +64,19 @@ public class FirstArrivedChargeStrategy<E>
         }
         else
         {
-            firstFound.await();
+            ChargeState<E> state;
 
-            return Collections.singletonList( getFutureResult( first ) );
+            synchronized ( states )
+            {
+                if ( ( state = states.get( charge ) ) == null )
+                {
+                    states.put( charge, ( state = new ChargeState() ) );
+                }
+            }
+
+            state.latch.await();
+
+            return Collections.singletonList( getFutureResult( state.wrapper ) );
         }
     }
 
